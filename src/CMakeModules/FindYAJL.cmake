569425d3ea4138a8ee247d88690cf01cd921be4a
Herb Kuta <herb.kuta@palm.com>
2012-09-05 16:58:15 -0700

    Clean up finding of dependent packages
    - Rewrite FindYAJL.cmake to use FindPackageHandleStandardArgs.cmake
      instead of LibFindMacros.cmake .
    - Use pkg_check_modules() to find libpcre.
    - Remove test for local copy of libpcre.
    - Remove unused Find*.cmake files and LibFindMacros.cmake .
    
    Open-webOS-DCO-1.0-Signed-off-by: Herb Kuta <herb.kuta@palm.com>
    Change-Id: I5802ae9455837129573f94b414829c9673d943d6

--- src/CMakeModules/FindYAJL.cmake
+++ src/CMakeModules/FindYAJL.cmake
@@ -1,26 +1,46 @@
-# Try to find pbnjson library
-# This will define
+# @@@LICENSE
+#
+#      Copyright (c) 2012 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License looks.
+#
+# LICENSE@@@
+
+# This will define:
 # YAJL_FOUND - system has yajl
 # YAJL_INCLUDE_DIRS - include directories necessary to compile w/ yajl
 # YAJL_LIBRARIES - libraries necessary to link to to get yajl
-include (LibFindMacros)
+#
+# If YAJL_STATIC is set, find the static library; otherwise, find the shared library.
+#
 
-#set(YAJL_INCLUDE_DIR $ENV{MY_YAJL_INCLUDE_PATH})
-#set(YAJL_LIBRARY $ENV{MY_YAJL_LIB_PATH})
+include(FindPackageHandleStandardArgs)
 
-# Include directories
-find_path(YAJL_INCLUDE_DIR NAMES yajl/yajl_parse.h yajl/yajl_gen.h yajl/yajl_common.h PATHS $ENV{MY_YAJL_INCLUDE_PATH})
+# Find the include directories
+find_path(YAJL_INCLUDE_DIRS NAMES yajl/yajl_parse.h yajl/yajl_gen.h yajl/yajl_common.h)
 
 # Find the library
-if (YAJL_STATIC)
-	find_library(YAJL_LIBRARY NAMES yajl_s PATHS $ENV{MY_YAJL_LIB_PATH})
-else ()
-	find_library(YAJL_LIBRARY NAMES yajl PATHS $ENV{MY_YAJL_LIB_PATH})
-endif ()
+if(YAJL_STATIC)
+	find_library(YAJL_LIBRARIES NAMES yajl_s)
+else()
+	find_library(YAJL_LIBRARIES NAMES yajl)
+endif()
 
-# let LibFindMacros take care of the rest
-set(YAJL_PROCESS_INCLUDES YAJL_INCLUDE_DIR)
-set(YAJL_PROCESS_LIBS YAJL_LIBRARY)
+find_package_handle_standard_args(YAJL DEFAULT_MSG YAJL_INCLUDE_DIRS YAJL_LIBRARIES)
 
-# LibFindMacros takes care of the rest
-libfind_process(YAJL)
+if(YAJL_FOUND)
+	mark_as_advanced(YAJL_INCLUDE_DIRS YAJL_LIBRARIES)
+else()
+	unset(YAJL_INCLUDE_DIRS)
+	unset(YAJL_LIBRARIES)
+endif()
