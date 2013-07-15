71a41e0fe4b49ab792435aabb5747deecabddc29
Anatolii Sakhnik <anatolii.sakhnik@lge.com>
2013-07-15 11:22:45 +0300

    Fix cosmetic whitespace style
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    Build succeeded
    
    :QA Notes:
    
    :Issues Addressed:
    [GF-6969] Refactor jobject to use GHashTable
    
    Open-webOS-DCO-1.0-Signed-off-by: Anatolii Sakhnik <anatolii.sakhnik@lge.com>
    
    Change-Id: I541445c6652c95342b3c90e44c963556b261a77f
    Reviewed-on: https://g2g.palm.com/2862
    Reviewed-by: Build Verification
    Reviewed-by: Anatolii Sakhnik <anatolii.sakhnik@lge.com>
    Tested-by: Anatolii Sakhnik <anatolii.sakhnik@lge.com>
    Reviewed-by: Alekseyev Oleksandr <alekseyev.oleksandr@lge.com>
    Tested-by: Alekseyev Oleksandr <alekseyev.oleksandr@lge.com>

--- src/test/cxx_api/TestRegression.cpp
+++ src/test/cxx_api/TestRegression.cpp
@@ -23,128 +23,128 @@ using namespace std;
 using namespace pbnjson;
 
 class NOV99444
-    : public testing::Test
+	: public testing::Test
 {
 protected:
-    string _faulty_schema;
-    string _valid_schema;
-    string _input;
+	string _faulty_schema;
+	string _valid_schema;
+	string _input;
 
-    virtual void SetUp()
-    {
-        _faulty_schema =
-        "{"
-            "\"type\" : \"object\","
-            "\"properties\" : {"
-                "\"errorCode\" : {"
-                    "\"type\" : \"integer\";"
-                "}"
-            "}"
-        "}";
+	virtual void SetUp()
+	{
+		_faulty_schema =
+			"{"
+			    "\"type\" : \"object\","
+			    "\"properties\" : {"
+			        "\"errorCode\" : {"
+			            "\"type\" : \"integer\";"
+			        "}"
+			    "}"
+			"}";
 
-        _valid_schema =
-        "{"
-            "\"type\" : \"object\","
-            "\"properties\" : {"
-                "\"errorCode\" : {"
-                        "\"type\" : \"integer\""
-                "}"
-            "}"
-        "}";
+		_valid_schema =
+			"{"
+			    "\"type\" : \"object\","
+			    "\"properties\" : {"
+			        "\"errorCode\" : {"
+			                "\"type\" : \"integer\""
+			        "}"
+			    "}"
+			"}";
 
-        JValue obj = Object()
-                   << JValue::KeyValue("returnValue", false)
-                   << JValue::KeyValue("errorCode", -1)
-                   << JValue::KeyValue("errorText", "Launch helper exited with unknown return code 0");
-        ASSERT_TRUE(JGenerator{}.toString(obj, JSchemaFragment("{}"), _input));
-    }
+		JValue obj = Object()
+		           << JValue::KeyValue("returnValue", false)
+		           << JValue::KeyValue("errorCode", -1)
+		           << JValue::KeyValue("errorText", "Launch helper exited with unknown return code 0");
+		ASSERT_TRUE(JGenerator{}.toString(obj, JSchemaFragment("{}"), _input));
+	}
 };
 
 TEST_F(NOV99444, Success)
 {
-    JSchema schema = JSchemaFragment(_valid_schema);
-    JDomParser parser;
+	JSchema schema = JSchemaFragment(_valid_schema);
+	JDomParser parser;
 
-    for (int i = 0; i < 10; i++)
-        parser.parse(_input, schema);
-    ASSERT_TRUE(parser.parse(_input, schema));
+	for (int i = 0; i < 10; i++)
+		parser.parse(_input, schema);
+	ASSERT_TRUE(parser.parse(_input, schema));
 
-    JValue json = parser.getDom();
+	JValue json = parser.getDom();
 
-    int error_code;
-    EXPECT_EQ(CONV_OK, json["errorCode"].asNumber<int>(error_code));
-    EXPECT_TRUE(json["errorText"].isString());
+	int error_code;
+	EXPECT_EQ(CONV_OK, json["errorCode"].asNumber<int>(error_code));
+	EXPECT_TRUE(json["errorText"].isString());
 }
 
 TEST_F(NOV99444, Failure)
 {
-    JSchema schema{JSchemaFragment(_faulty_schema)};
-    JDomParser parser;
+	JSchema schema{JSchemaFragment(_faulty_schema)};
+	JDomParser parser;
 
-    for (int i = 0; i < 10; i++)
-        parser.parse(_input, schema);
-    ASSERT_FALSE(parser.parse(_input, schema));
+	for (int i = 0; i < 10; i++)
+		parser.parse(_input, schema);
+	ASSERT_FALSE(parser.parse(_input, schema));
 }
 
 TEST(SysmgrFailure, Parse)
 {
-    // Schema from sysmgr crash
-    string valid_schema = "{\"type\":\"object\",\"properties\":{\"quicklaunch\":{\"type\":\"boolean\",\"optional\":true},\"launcher\":{\"type\":\"boolean\",\"optional\":true},\"universal search\":{\"type\":\"boolean\",\"optional\":true}},\"additionalProperties\":false}";
-    JValue obj = Object()
-                 << JValue::KeyValue("universal search", false)
-                 << JValue::KeyValue("launcher", false);
-    string input;
-    ASSERT_TRUE(JGenerator{}.toString(obj, JSchemaFragment("{}"), input));
-    EXPECT_TRUE(JDomParser{}.parse(input, JSchemaFragment{valid_schema}));
+	// Schema from sysmgr crash
+	string valid_schema = "{\"type\":\"object\",\"properties\":{\"quicklaunch\":{\"type\":\"boolean\",\"optional\":true},\"launcher\":{\"type\":\"boolean\",\"optional\":true},\"universal search\":{\"type\":\"boolean\",\"optional\":true}},\"additionalProperties\":false}";
+	JValue obj = Object()
+	             << JValue::KeyValue("universal search", false)
+	             << JValue::KeyValue("launcher", false);
+	string input;
+	ASSERT_TRUE(JGenerator{}.toString(obj, JSchemaFragment("{}"), input));
+	EXPECT_TRUE(JDomParser{}.parse(input, JSchemaFragment{valid_schema}));
 }
 
 TEST(GF_7251, MemoryLeak)
 {
-    const pbnjson::JSchemaFragment schema("{}");
+	const pbnjson::JSchemaFragment schema("{}");
 
-    string serialized;
-    ASSERT_TRUE(JGenerator{}.toString(pbnjson::Object(), schema, serialized));
-    EXPECT_STREQ("{}", serialized.c_str());
+	string serialized;
+	ASSERT_TRUE(JGenerator{}.toString(pbnjson::Object(), schema, serialized));
+	EXPECT_STREQ("{}", serialized.c_str());
 }
 
 TEST(GF_7251, MemoryLeak2)
 {
-    char const *schema = "{\"type\": \"object\","
-                           "\"properties\": {"
-                             "\"key\": {"
-                               "\"type\" : \"boolean\""
-                             "}"
-                           "}"
-                         "}";
-    JValue obj = Object() << JValue::KeyValue("key", "true");
-    string serialized;
-    ASSERT_FALSE(JGenerator{}.toString(obj, JSchemaFragment{schema}, serialized));
-    EXPECT_STREQ("", serialized.c_str());
+	char const *schema = "{\"type\": \"object\","
+	                       "\"properties\": {"
+	                         "\"key\": {"
+	                           "\"type\" : \"boolean\""
+	                         "}"
+	                       "}"
+	                     "}";
+	JValue obj = Object() << JValue::KeyValue("key", "true");
+	string serialized;
+	ASSERT_FALSE(JGenerator{}.toString(obj, JSchemaFragment{schema}, serialized));
+	EXPECT_STREQ("", serialized.c_str());
 }
 
 TEST(GF_7251, MemoryLeak3)
 {
-    char const *schema =
-        "{ \"type\": \"object\","
-          "\"properties\": {"
-            "\"hasVideo\":{"
-              "\"optional\":true,"
-              "\"type\": \"object\","
-              "\"properties\": {"
-                "\"state\": {\"type\":\"boolean\"},"
-                "\"mediaId\": {\"type\": \"string\"}"
-              "}"
-            "}"
-          "}"
-        "}";
+	char const *schema =
+		"{ \"type\": \"object\","
+		  "\"properties\": {"
+		    "\"hasVideo\":{"
+		      "\"optional\":true,"
+		      "\"type\": \"object\","
+		      "\"properties\": {"
+		        "\"state\": {\"type\":\"boolean\"},"
+		        "\"mediaId\": {\"type\": \"string\"}"
+		      "}"
+		    "}"
+		  "}"
+		"}";
 
-    JDomParser parser;
-    ASSERT_TRUE(parser.parse("{\"hasVideo\":{\"state\":1,\"mediaId\":\"jtNe8Pn3csslQ6R\"}}", JSchemaFragment("{}")));
+	JDomParser parser;
+	ASSERT_TRUE(parser.parse("{\"hasVideo\":{\"state\":1,\"mediaId\":\"jtNe8Pn3csslQ6R\"}}", JSchemaFragment("{}")));
 
-    JValue object = parser.getDom();
-    string serialized;
+	JValue object = parser.getDom();
+	string serialized;
 
-    EXPECT_FALSE(JGenerator{}.toString(object, JSchemaFragment{schema}, serialized));
+	EXPECT_FALSE(JGenerator{}.toString(object, JSchemaFragment{schema}, serialized));
 }
 
-// vim: set et ts=4 sw=4:
+// vim: set noet ts=4 sw=4:
