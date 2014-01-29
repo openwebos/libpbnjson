809c4e3056d732b2e7f39d072d5aaf480459c839
Nikolay Orliuk <nikolay.orliuk@lge.com>
2014-01-29 13:50:25 +0200

    Enhance benchmarking
    :Release Notes:
    Better reproducibility of benchmarking tests
    
    :Detailed Notes:
    - process speedup by avoiding extra cycle if warmup already hit the
      measure_time
    - tolerance to system load by using CPU usage time if available
    - tolerance to spikes by using median instead of average
    
    :Testing Performed:
    gtest
    
    :QA Notes:
    
    :Issues Addressed:
    [BHV-123] pbnjson: Improve performance of json parsing with YAJL
    
    Open-webOS-DCO-1.0-Signed-off-by: Nikolay Orliuk <nikolay.orliuk@lge.com>
    Open-webOS-DCO-1.0-Signed-off-by: Andrii Zora <andrii.zora@lge.com>
    
    Change-Id: I661407ce08ef807361cb481578674f845c18570e
    Reviewed-on: https://g2g.palm.com/4540
    Reviewed-by: DCO Verification
    Reviewed-by: Nikolay Orliuk <nikolay.orliuk@lge.com>
    Tested-by: Nikolay Orliuk <nikolay.orliuk@lge.com>
    Reviewed-by: Alekseyev Oleksandr <alekseyev.oleksandr@lge.com>
    Reviewed-by: Anatolii Sakhnik <anatolii.sakhnik@lge.com>

--- src/test/c_api/PerformanceUtils.hpp
+++ src/test/c_api/PerformanceUtils.hpp
@@ -1,6 +1,6 @@
 // @@@LICENSE
 //
-//      Copyright (c) 2009-2013 LG Electronics, Inc.
+//      Copyright (c) 2009-2014 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,35 +16,91 @@
 //
 // LICENSE@@@
 
-#include <chrono>
+#include <ctime>
 #include <memory>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
-using namespace std::chrono;
 
-double BenchmarkMeasure(function<void(size_t)> code, size_t n)
+static clockid_t ChooseBenchmarkClock()
 {
-	typedef high_resolution_clock ClockT;
-	time_point<ClockT> start = ClockT::now();
-	code(n);
-	time_point<ClockT> finish = ClockT::now();
-	duration<double> time_span = duration_cast<duration<double>>(finish - start);
-	return time_span.count();
+	clockid_t clock;
+	if (clock_getcpuclockid(0, &clock) != 0)
+	{
+		// no CPU time, fallback to monotonic
+		return CLOCK_MONOTONIC;
+	}
+	return clock;
 }
 
+static double BenchmarkMeasure(function<void(size_t)> code, size_t n)
+{
+	static const clockid_t clock = ChooseBenchmarkClock();
+	timespec start, finish;
+	int zero0 = clock_gettime(clock, &start);
+	code(n);
+	int zero1 = clock_gettime(clock, &finish);
+	assert( zero0 == 0 );
+	assert( zero1 == 0 );
+	if (zero0 != 0 || zero1 != 0) abort();
+	return (finish.tv_sec - start.tv_sec) + (finish.tv_nsec - start.tv_nsec)*1e-9;
+}
+
+double Median(std::vector<double> &samples)
+{
+	assert( !samples.empty() );
+	size_t m = samples.size()/2;
+	double median;
+	std::nth_element(samples.begin(), samples.begin() + m, samples.end());
+	median = samples[m];
+	if ((samples.size() % 2) == 0)
+	{
+		std::nth_element(samples.begin(), samples.begin() + m - 1, samples.end());
+		median = (median + samples[m-1])/2;
+	}
+	return median;
+}
+
+// benchmark scalable code function (linearity is required)
 double BenchmarkPerformNs(function<void(size_t)> code)
 {
-	const double low_time_threshold = 3; // seconds, assume a good good resolution warmup
-	const double measure_seconds = 5;
+	const double low_time_threshold = 1; // seconds, assume a good good resolution warmup
+	const double measure_seconds = 15; // quarter of minute
+	const double proximity = 0.05; // +5% to ensure boundary cross
+	const size_t samples_count = 5; // 5 samples by ~3 seconds each
+
+	const double sample_time_mark = measure_seconds / samples_count;
+	const double sample_time = (1+proximity) * sample_time_mark;
+
+	vector<double> samples;
+	samples.reserve(samples_count);
+
+	// for first element we do warmup
 	size_t n = 50;
 	double t;
-	do
+	while ((t = BenchmarkMeasure(code, n)) < sample_time_mark)
 	{
-		n*=2;
-	} while ((t = BenchmarkMeasure(code, n)) < low_time_threshold);
+		if (t < low_time_threshold) n *= 2;
+		else n = sample_time * n / t;
+	}
+	samples.push_back(t * 1e9 / n);
 
-	// actual measure
-	size_t repeats = measure_seconds * n / t;
-	double timing = BenchmarkMeasure(code, repeats);
-	return timing * 1e9 / repeats;
+	// for rest we do only slight adjustment
+	size_t total_n = n;
+	double total_t = t;
+	for (size_t i = 1; i < samples_count; ++i)
+	{
+		do
+		{
+			// adjust n according to current average
+			n = sample_time * total_n / total_t;
+			t = BenchmarkMeasure(code, n);
+
+			// linearity
+			total_n += n, total_t += t;
+		} while (t < sample_time_mark); // take only values with a good resolution
+		samples.push_back(t * 1e9 / n);
+	}
+	return Median(samples);
 }
